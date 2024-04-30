f1e8f61304975a71fdda93f402d095df4377ea87
SarikaBandewar <shabs23119@yahoo.co.in>
2024-04-30 21:59:28 +0530

    adding JWT security - intermediate checkin

---
+++ src/main/java/com/sk/userservice/security/repositories/ClientRepository.java
@@ -0,0 +1,13 @@
+package com.sk.userservice.security.repositories;
+
+import java.util.Optional;
+
+
+import com.sk.userservice.security.models.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClientRepository extends JpaRepository<Client, String> {
+    Optional<Client> findByClientId(String clientId);
+}
